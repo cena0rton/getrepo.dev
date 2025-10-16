1f69673cf1e0a15added6e407f4193f24eb243ab
cena0rton <cena0rton@users.noreply.github.com>
2025-10-16 15:20:02 +0530

    Update dashboard component styling

--- app/src/app/dashboard/Dash.tsx
+++ app/src/app/dashboard/Dash.tsx
@@ -180,13 +180,13 @@ export default function Dash() {
               value={query}
               onChange={(e) => setQuery(e.target.value)}
               placeholder="Search (e.g. nextjs, express, typescript)"
-              className="w-full border bg-linear-to-r from-neutral-100 to-neutral-50 dark:bg-linear-to-tr dark:from-neutral-800 dark:to-neutral-950 dark:border-[#6366f1]/40 border-neutral-200 rounded px-3 py-2 outline-none dark:text-neutral-300 text-neutral-800 focus:ring-1 focus:ring-neutral-300 dark:focus:ring-[#6366f1]/40 transition-all duration-100 placeholder:text-neutral-400 "
+              className="w-full border bg-linear-to-r from-neutral-100 to-neutral-50 dark:bg-linear-to-tr dark:from-neutral-800 dark:to-neutral-950 dark:border-[#6366f1]/20 border-neutral-200 rounded px-3 py-2 outline-none dark:text-neutral-300 text-neutral-800 focus:ring-1 focus:ring-neutral-300 dark:focus:ring-[#6366f1]/40 transition-all duration-100 placeholder:text-neutral-400 "
             />
             <div className="flex justify-start items-center md:grid md:grid-cols-[1fr_140px_140px_120px] gap-2 w-full mx-auto md:overflow-x-visible">
             <select
               value={language}
               onChange={(e) => setLanguage(e.target.value)}
-              className="border dark:border-[#6366f1]/40 border-neutral-200 rounded px-2 py-2 bg-linear-to-r from-neutral-100 to-neutral-50 dark:bg-linear-to-tr dark:from-neutral-800 dark:to-neutral-950 outline-none dark:text-neutral-300 w-36 "
+              className="border dark:border-[#6366f1]/20 border-neutral-200 rounded px-2 py-2 bg-linear-to-r from-neutral-100 to-neutral-50 dark:bg-linear-to-tr dark:from-neutral-800 dark:to-neutral-950 outline-none dark:text-neutral-300/80 w-36 "
             >
               <option value="" className="text-neutral-300">All languages</option>
               <option value="JavaScript">JavaScript</option>
@@ -203,7 +203,7 @@ export default function Dash() {
             <select
               value={sort}
               onChange={(e) => setSort(e.target.value)}
-              className="border border-neutral-200 dark:border-[#6366f1]/40 rounded px-2 py-2 bg-linear-to-r from-neutral-100 to-neutral-50 dark:bg-linear-to-tr dark:from-neutral-800 dark:to-neutral-950 outline-none dark:text-neutral-300  w-36 "
+              className="border border-neutral-200 dark:border-[#6366f1]/20 rounded px-2 py-2 bg-linear-to-r from-neutral-100 to-neutral-50 dark:bg-linear-to-tr dark:from-neutral-800 dark:to-neutral-950 outline-none dark:text-neutral-300/80  w-36 "
             >
               <option value="stars" className="text-neutral-300">Sort: Stars</option>
               <option value="forks" className="text-neutral-300">Sort: Forks</option>
@@ -212,7 +212,7 @@ export default function Dash() {
             <select
               value={order}
               onChange={(e) => setOrder(e.target.value)}
-              className="border-1 border-neutral-200 dark:border-[#6366f1]/40 rounded px-2 py-2 w-24 bg-linear-to-r from-neutral-100 to-neutral-50 dark:bg-linear-to-tr dark:from-neutral-800 dark:to-neutral-950 outline-none inset-shadow-md inset-shadow-blue-400 dark:text-neutral-300 "
+              className="border-1 border-neutral-200 dark:border-[#6366f1]/20 rounded px-2 py-2 w-24 bg-linear-to-r from-neutral-100 to-neutral-50 dark:bg-linear-to-tr dark:from-neutral-800 dark:to-neutral-950 outline-none inset-shadow-md inset-shadow-blue-400 dark:text-neutral-300/80 "
             >
               <option value="desc">Desc</option>
               <option value="asc">Asc</option>
